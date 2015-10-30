6d55c4206f0ae799700dc0f99a36e885c2eca232
Johan Dahlin <johan.dahlin@liu.se>
2015-10-30 13:22:22 +0100

    updated code when revising the tutorial
    Former-commit-id: 3dd1310560bfdde7f23ddb0ad9eb5cb06d04f1df [formerly 3dd1310560bfdde7f23ddb0ad9eb5cb06d04f1df [formerly 5768bd96a6c0e09c0864ed14359bf217f0287777]]
    Former-commit-id: a7e0f6ed90597448b38d8fd4639ab22380d9a418
    Former-commit-id: 03ec09f5abe154e4080c30f7bd61dea32bf11acb

--- r/example2-lgss.R
+++ r/example2-lgss.R
@@ -58,11 +58,10 @@ nPart    = 100;
 nBurnIn  = 1000;
 nIter    = 5000;
 
-# The standard deviation in the random walk proposal
-stepSize = 0.10;
-
 # Run the PMH algorithm
-res = pmh(data$y,initPar,sigmav,sigmae,nPart,T,x0,nIter,stepSize)
+res1 = pmh(data$y,initPar,sigmav,sigmae,nPart,T,x0,nIter,stepSize = 0.01)
+res2 = pmh(data$y,initPar,sigmav,sigmae,nPart,T,x0,nIter,stepSize = 0.10)
+res3 = pmh(data$y,initPar,sigmav,sigmae,nPart,T,x0,nIter,stepSize = 0.50)
 
 ##############################################################################
 # Plot the results
@@ -71,32 +70,66 @@ res = pmh(data$y,initPar,sigmav,sigmae,nPart,T,x0,nIter,stepSize)
 # Export plot to file
 #cairo_pdf("example2-lgss.pdf", height = 10, width = 8)
 
-# Plot the parameter posterior estimate
-# Solid black line indicate posterior mean
-layout(matrix(1:3, 3, 1, byrow = TRUE)); par(mar=c(4,5,0,0))
-hist(res[nBurnIn:nIter], breaks=floor(sqrt(nIter-nBurnIn)), col=rgb(t(col2rgb("#7570B3"))/256,alpha=0.25),border=NA,xlab=expression(phi),ylab="posterior estimate",main="",xlim=c(0.5,0.8),ylim=c(0,12),freq=F)
-lines(density(res[nBurnIn:nIter],kernel="e",from=0.5,to=0.8),lwd=2,col="#7570B3")
-lines(seq(0.5,1.0,0.01),dnorm(seq(0.5,1.0,0.01),0,1),lwd=1,col="darkgrey")
-abline(v=mean(res[nBurnIn:nIter]),lwd=1,lty="dotted")
+layout(matrix(1:9, 3, 3, byrow = TRUE)); par(mar=c(4,5,0,0))
 
-# Plot the trace of the Markov chain after burn-in
-# Solid black line indicate posterior mean
-plot(seq(nBurnIn,nIter,1),res[nBurnIn:nIter],col = '#7570B3', type="l",xlab="iteration",ylab=expression(phi),bty="n",ylim=c(0.5,0.8))
-grid=seq(nBurnIn,nIter,1)
-polygon(c(grid,rev(grid)),c(res[nBurnIn:nIter],rep(0,length(res[nBurnIn:nIter]))),border=NA,col=rgb(t(col2rgb("#7570B3"))/256,alpha=0.25))
-abline(h=mean(res[nBurnIn:nIter]),lwd=1,lty="dotted")
+# Plot the parameter posterior estimate
+hist(res1[nBurnIn:nIter], breaks=floor(sqrt(nIter-nBurnIn)), col=rgb(t(col2rgb("#7570B3"))/256,alpha=0.25),border=NA,xlab=expression(phi),ylab="posterior estimate",main="",xlim=c(0.5,0.8),ylim=c(0,12),freq=F)
+lines(density(res1[nBurnIn:nIter],kernel="e",from=0.5,to=0.8),lwd=2,col="#7570B3")
+lines(seq(0.5,1.0,0.01),dnorm(seq(0.5,1.0,0.01),0,1),lwd=1,col="darkgrey")
+abline(v=mean(res1[nBurnIn:nIter]),lwd=1,lty="dotted")
+
+hist(res2[nBurnIn:nIter], breaks=floor(sqrt(nIter-nBurnIn)), col=rgb(t(col2rgb("#E7298A"))/256,alpha=0.25),border=NA,xlab=expression(phi),ylab="posterior estimate",main="",xlim=c(0.5,0.8),ylim=c(0,12),freq=F)
+lines(density(res2[nBurnIn:nIter],kernel="e",from=0.5,to=0.8),lwd=2,col="#E7298A")
+lines(seq(0.5,1.0,0.01),dnorm(seq(0.5,1.0,0.01),0,1),lwd=1,col="darkgrey")
+abline(v=mean(res2[nBurnIn:nIter]),lwd=1,lty="dotted")
+
+hist(res3[nBurnIn:nIter], breaks=floor(sqrt(nIter-nBurnIn)), col=rgb(t(col2rgb("#66A61E"))/256,alpha=0.25),border=NA,xlab=expression(phi),ylab="posterior estimate",main="",xlim=c(0.5,0.8),ylim=c(0,12),freq=F)
+lines(density(res3[nBurnIn:nIter],kernel="e",from=0.5,to=0.8),lwd=2,col="#66A61E")
+lines(seq(0.5,1.0,0.01),dnorm(seq(0.5,1.0,0.01),0,1),lwd=1,col="darkgrey")
+abline(v=mean(res3[nBurnIn:nIter]),lwd=1,lty="dotted")
+
+# Plot the trace of the Markov chain during 1000 iterations after the burn-in
+plot(seq(nBurnIn,nBurnIn+1000,1),res1[nBurnIn:(nBurnIn+1000)],col = '#7570B3', type="l",xlab="iteration",ylab=expression(phi),bty="n",ylim=c(0.5,0.8))
+grid=seq(nBurnIn,nBurnIn+1000,1)
+polygon(c(grid,rev(grid)),c(res1[nBurnIn:(nBurnIn+1000)],rep(0,length(res1[nBurnIn:(nBurnIn+1000)]))),border=NA,col=rgb(t(col2rgb("#7570B3"))/256,alpha=0.25))
+abline(h=mean(res1[nBurnIn:nIter]),lwd=1,lty="dotted")
+
+plot(seq(nBurnIn,nBurnIn+1000,1),res2[nBurnIn:(nBurnIn+1000)],col = '#E7298A', type="l",xlab="iteration",ylab=expression(phi),bty="n",ylim=c(0.5,0.8))
+grid=seq(nBurnIn,nBurnIn+1000,1)
+polygon(c(grid,rev(grid)),c(res2[nBurnIn:(nBurnIn+1000)],rep(0,length(res2[nBurnIn:(nBurnIn+1000)]))),border=NA,col=rgb(t(col2rgb("#E7298A"))/256,alpha=0.25))
+abline(h=mean(res2[nBurnIn:nIter]),lwd=1,lty="dotted")
+
+plot(seq(nBurnIn,nBurnIn+1000,1),res3[nBurnIn:(nBurnIn+1000)],col = '#66A61E', type="l",xlab="iteration",ylab=expression(phi),bty="n",ylim=c(0.5,0.8))
+grid=seq(nBurnIn,nBurnIn+1000,1)
+polygon(c(grid,rev(grid)),c(res3[nBurnIn:(nBurnIn+1000)],rep(0,length(res3[nBurnIn:(nBurnIn+1000)]))),border=NA,col=rgb(t(col2rgb("#66A61E"))/256,alpha=0.25))
+abline(h=mean(res3[nBurnIn:nIter]),lwd=1,lty="dotted")
 
 # Plot the ACF of the Markov chain
-foo=acf( res[nBurnIn:nIter], plot=F)
-plot(foo$lag,foo$acf,col = '#7570B3', type="l",xlab="iteration",ylab="ACF",bty="n",lwd=2)
+foo=acf( res1[nBurnIn:nIter], plot=F, lag.max=60)
+plot(foo$lag,foo$acf,col = '#7570B3', type="l",xlab="iteration",ylab="ACF",bty="n",lwd=2,ylim=c(-0.2,1))
 polygon(c(foo$lag,rev(foo$lag)),c(foo$acf,rep(0,length(foo$acf))),border=NA,col=rgb(t(col2rgb("#7570B3"))/256,alpha=0.25))
+abline(h=1.96/sqrt(nIter-nBurnIn),lty="dotted")
+abline(h=-1.96/sqrt(nIter-nBurnIn),lty="dotted")
+
+foo=acf( res2[nBurnIn:nIter], plot=F, lag.max=60)
+plot(foo$lag,foo$acf,col = '#E7298A', type="l",xlab="iteration",ylab="ACF",bty="n",lwd=2,ylim=c(-0.2,1))
+polygon(c(foo$lag,rev(foo$lag)),c(foo$acf,rep(0,length(foo$acf))),border=NA,col=rgb(t(col2rgb("#E7298A"))/256,alpha=0.25))
+abline(h=1.96/sqrt(nIter-nBurnIn),lty="dotted")
+abline(h=-1.96/sqrt(nIter-nBurnIn),lty="dotted")
+
+
+foo=acf( res3[nBurnIn:nIter], plot=F, lag.max=60)
+plot(foo$lag,foo$acf,col = '#66A61E', type="l",xlab="iteration",ylab="ACF",bty="n",lwd=2,ylim=c(-0.2,1))
+polygon(c(foo$lag,rev(foo$lag)),c(foo$acf,rep(0,length(foo$acf))),border=NA,col=rgb(t(col2rgb("#66A61E"))/256,alpha=0.25))
 abline(h=1.96/sqrt(nIter-nBurnIn),lty="dotted")
 abline(h=-1.96/sqrt(nIter-nBurnIn),lty="dotted")
 
 #dev.off()
 
 # Estimate the parameter posterior mean
-mean( res[nBurnIn:nIter] )
+mean( res1[nBurnIn:nIter] )
+mean( res2[nBurnIn:nIter] )
+mean( res3[nBurnIn:nIter] )
 
 ##############################################################################
 # End of file
