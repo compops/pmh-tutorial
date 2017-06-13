bf52e9f514a7259e12f73b18437999f4d1154d94
Johan Dahlin <github@johandahlin.com>
2017-06-13 14:58:21 +0200

    use Python3 print and division.
    Former-commit-id: b1a3914e492edbfe4085154f1b6de9cb533c7ad0

--- python/helpers/parameterEstimation.py
+++ python/helpers/parameterEstimation.py
@@ -20,6 +20,8 @@
 #
 ##############################################################################
 
+from __future__ import print_function, division
+
 import numpy as np
 from numpy.random import randn, uniform, multivariate_normal
 from scipy.stats import gamma, norm
