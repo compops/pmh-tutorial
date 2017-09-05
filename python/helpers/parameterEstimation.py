f9289a2689a69cfced30f5c42531141c367680bb
Johan Dahlin <github@johandahlin.com>
2017-09-05 10:44:33 +1000

    refactorizing code for readability.
    Former-commit-id: e62f4ee7a8fb9e08283a373c7014cd5194637764 [formerly e62f4ee7a8fb9e08283a373c7014cd5194637764 [formerly 16ac32e8757abf7423ea2203d052f06f56a490c7]]
    Former-commit-id: 3dc300030e7d4db27f3dc5c7c4453bf43984a9af
    Former-commit-id: 1353b1ea0bfcee0bd412fd465d6e06d75ff94b5f

--- python/helpers/parameterEstimation.py
+++ python/helpers/parameterEstimation.py
@@ -1,62 +1,15 @@
-##############################################################################
-#
 # Particle Metropolis-Hastings for LGSS and SV models
-#
-# Copyright (C) 2017 Johan Dahlin < liu (at) johandahlin.com.nospam >
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-##############################################################################
 
 from __future__ import print_function, division
-
 import numpy as np
 from numpy.random import randn, uniform, multivariate_normal
 from scipy.stats import gamma, norm
 
-#############################################################################
 # Particle Metropolis-Hastings (PMH) for the LGSS model
-#
-# Inputs:
-# y:                   observations from the system for t=1,...,T.
-#
-# initialPhi:          initial value for phi (persistence of the state)
-#
-# theta:               the standard deviations of the state noise theta[1]
-#                      and observation noise theta[2].
-#
-# noParticles:         number of particles (N)
-#
-# initialState:        the initial state.
-#
-# particleFilter:      function for estimating the likelihood
-#
-# noIterations:        the number of iterations in PMH 
-# 
-# stepSize:            the standard deviation of the RW proposal.
-#
-# Output:
-# phi:                 K samples from the parameter posterior for phi.
-#
-#
-#############################################################################
+def particleMetropolisHastings(
+    observations, initialPhi, parameters, noParticles, 
+    initialState, particleFilter, noIterations, stepSize):
 
-
-def particleMetropolisHastings(y, initialPhi, theta, noParticles, initialState, particleFilter, noIterations, stepSize):
-
-    # Initalise variables
     phi = np.zeros(noIterations)
     phiProposed = np.zeros(noIterations)
     logLikelihood = np.zeros(noIterations)
@@ -65,11 +18,9 @@ def particleMetropolisHastings(y, initialPhi, theta, noParticles, initialState, 
 
     # Set the initial parameter and estimate the initial log-likelihood
     phi[0] = initialPhi
-    _, logLikelihood[0] = particleFilter(y, (phi[0], theta[1], theta[2]), noParticles, initialState)
+    _, logLikelihood[0] = particleFilter(observations, (phi[0], parameters[1], parameters[2]), noParticles, initialState)
 
-    #=====================================================================
-    # Run main loop
-    #=====================================================================
+    # Main loop
     for k in range(1, noIterations):
 
         # Propose a new parameter
@@ -77,18 +28,14 @@ def particleMetropolisHastings(y, initialPhi, theta, noParticles, initialState, 
 
         # Estimate the log-likelihood if the proposed phi results in a stable model
         if (np.abs(phiProposed[k]) < 1.0):
-            _, logLikelihoodProposed[k] = particleFilter(y, (phiProposed[k], theta[1], theta[2]), noParticles, initialState)
+            _, logLikelihoodProposed[k] = particleFilter(observations, (phiProposed[k], parameters[1], parameters[2]), noParticles, initialState)
 
         # Compute the acceptance probability
         acceptProbability = np.min((1.0, np.exp(logLikelihoodProposed[k] - logLikelihood[k - 1])))
-        
-        # Set the acceptance probability to zero if the proposed phi results in an unstable model
         acceptProbability *= np.abs(phiProposed[k]) < 1.0
         
-        # Generate uniform random variable in U[0,1]
-        uniformRandomVariable = uniform()
-
         # Accept / reject step
+        uniformRandomVariable = uniform()
         if uniformRandomVariable < acceptProbability:
             # Accept the parameter
             phi[k] = phiProposed[k]
@@ -118,58 +65,27 @@ def particleMetropolisHastings(y, initialPhi, theta, noParticles, initialState, 
             print(
                 "##################################################################### ")
     
-    #=====================================================================
-    # Return traces of the parameters
-    #=====================================================================
     return phi
 
-#############################################################################
 # Particle Metropolis-Hastings (PMH) for the SV model
-#
-# Inputs:
-# y:                   observations from the system for t=1,...,T.
-#
-# initialTheta:        initial values for the parameters (mu, phi, sigmav)
-#
-#
-# noParticles:         number of particles (N)
-#
-# initialState:        the initial state.
-#
-# particleFilter:      function for estimating the likelihood
-#
-# noIterations:        the number of iterations in PMH
-#
-# stepSize:            the standard deviation of the RW proposal.
-#
-# Outputs:
-# xHatFiltered:        Estimate of the log-volatility
-# theta:               K samples from the parameter posterior.
-#
-#
-#############################################################################
+def particleMetropolisHastingsSVModel(
+    observations, initialTheta, noParticles, particleFilter, noIterations, stepSize):
 
+    noObservations = len(observations)
 
-def particleMetropolisHastingsSVModel(y, initialTheta, noParticles, particleFilter, noIterations, stepSize):
-
-    T = len(y)
-
-    # Initalise variables
     theta = np.zeros((noIterations, 3))
     thetaProposed = np.zeros((noIterations, 3))
     logLikelihood = np.zeros(noIterations)
     logLikelihoodProposed = np.zeros(noIterations)
-    xHatFiltered = np.zeros((noIterations, T))
-    xHatFilteredProposed = np.zeros((noIterations, T))
+    xHatFiltered = np.zeros((noIterations, noObservations))
+    xHatFilteredProposed = np.zeros((noIterations, noObservations))
     proposedThetaAccepted = np.zeros(noIterations)
 
     # Set the initial parameter and estimate the initial log-likelihood
-    theta[0, :] = initialTheta;
-    (xHatFiltered[0, :], logLikelihood[0]) = particleFilter(y, theta[0, :], noParticles);
+    theta[0, :] = initialTheta
+    (xHatFiltered[0, :], logLikelihood[0]) = particleFilter(observations, theta[0, :], noParticles)
 
-    #=====================================================================
-    # Run main loop
-    #=====================================================================
+    # Main loop
     for k in range(1, noIterations):
 
         # Propose a new parameter
@@ -178,7 +94,7 @@ def particleMetropolisHastingsSVModel(y, initialTheta, noParticles, particleFilt
 
         # Estimate the log-likelihood if the proposed theta results in a stable model
         if ((np.abs(thetaProposed[k, 1]) < 1.0) & (thetaProposed[k, 2] > 0.0)):
-            (xHatFilteredProposed[k, :], logLikelihoodProposed[k]) = particleFilter(y, thetaProposed[k, :], noParticles)
+            (xHatFilteredProposed[k, :], logLikelihoodProposed[k]) = particleFilter(observations, thetaProposed[k, :], noParticles)
 
         # Compute the ratio between the prior distributions (in log-form)
         prior = norm.logpdf(thetaProposed[k, 0], 0, 1) 
@@ -192,15 +108,11 @@ def particleMetropolisHastingsSVModel(y, initialTheta, noParticles, particleFilt
 
         # Compute the acceptance probability
         acceptProbability = np.min((1.0, np.exp(prior + logLikelihoodProposed[k] - logLikelihood[k - 1])))
-
-        # Set the acceptance probability to zero if the proposed theta results in an unstable model
         acceptProbability *= np.abs(thetaProposed[k, 1]) < 1.0
         acceptProbability *= thetaProposed[k, 2] > 0.0
 
-        # Generate uniform random variable in U[0,1]
-        uniformRandomVariable = uniform()
-
         # Accept / reject step
+        uniformRandomVariable = uniform()
         if (uniformRandomVariable < acceptProbability):
             # Accept the parameter
             theta[k, :] = thetaProposed[k, :]
@@ -232,7 +144,4 @@ def particleMetropolisHastingsSVModel(y, initialTheta, noParticles, particleFilt
             print(
                 "##################################################################### ")
     
-    #=====================================================================
-    # Return traces of the parameters
-    #=====================================================================
     return (xHatFiltered, theta)
--- python/helpers/stateEstimation.py
+++ python/helpers/stateEstimation.py
@@ -1,261 +1,130 @@
-##############################################################################
-#
-# State estimation in LGSS and SV models using Kalman and particle filters.
-#
-# Copyright (C) 2017 Johan Dahlin < liu (at) johandahlin.com.nospam >
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-##############################################################################
+## State estimation in LGSS and SV models using Kalman and particle filters.
 
 from __future__ import print_function, division
-
 import numpy as np
 from numpy.random import randn, choice
 from scipy.stats import norm
 
-
-##############################################################################
-##########################################################################
-#
 # Kalman filter for the linear Gaussian SSM
-#
-# Inputs:
-# y:                     observations from the system for t=1,...,T.
-#
-# theta:                 the persistence of the state theta[0] and the
-#                        standard deviations of the state noise theta[1] and
-#                        observation noise theta[2].
-#
-# initialState           the initial state 
-#
-# initialStateCovariance the initial state  covariance
-#
-# Outputs:
-# xHatFiltered:          vector with T elements
-#                        estimates of the filtered state
-#                        for each t=0,1,...,T-1.
-#
-# loglikelihood:         estimate of the log-likelihood at T-1
-#
-##########################################################################
-##############################################################################
-
-def kalmanFilter(y, theta, initialState, initialStateCovariance):
-
-    T = len(y)
-
-    # Initalise variables
-    predictiveCovariance = initialStateCovariance
-    xHatPredicted = initialState * np.ones((T + 1, 1))
-    xHatFiltered = initialState * np.ones((T, 1))
-
-    # Set parameters
-    A = theta[0]
+def kalmanFilter(observations, parameters, initialState, initialStateCov):
+    
+    noObservations = len(observations)
+    A = parameters[0]
     C = 1
-    Q = theta[1]**2
-    R = theta[2]**2
+    Q = parameters[1]**2
+    R = parameters[2]**2
 
-    # Run main loop
-    for t in range(0, T):
+    predictiveCov = initialStateCov
+    xHatPredicted = initialState * np.ones((noObservations + 1, 1))
+    xHatFiltered = initialState * np.ones((noObservations, 1))
 
-        # Calculate the Kalman Gain
-        S = C * predictiveCovariance * C + R
-        kalmanGain = predictiveCovariance * C / S
+    # Main loop
+    for t in range(0, noObservations):
 
-        # Compute the state estimate
-        yHatPredicted = C * xHatPredicted[t]
-        xHatFiltered[t] = xHatPredicted[t] + kalmanGain * (y[t - 1] - yHatPredicted)
+        # Correction step
+        S = C * predictiveCov * C + R
+        kalmanGain = predictiveCov * C / S
+        filteredCovariance = predictiveCov - kalmanGain * S * kalmanGain
+
+        yHatPredicted = C * xHatPredicted[t]    
+        xHatFiltered[t] = xHatPredicted[t] + kalmanGain * (observations[t - 1] - yHatPredicted)
+        
+        # Prediction step
         xHatPredicted[t + 1] = A * xHatFiltered[t]
-
-        # Update covariance
-        filteredCovariance = predictiveCovariance - kalmanGain * S * kalmanGain
-        predictiveCovariance = A * filteredCovariance * A + Q
+        predictiveCov = A * filteredCovariance * A + Q
 
     return xHatFiltered
 
-
-##############################################################################
-##############################################################################
-#
 # Fully-adapted particle filter for the linear Gaussian SSM
-#
-# Inputs:
-# y:                   observations from the system for t=1,...,T.
-#
-# theta:               the persistence of the state theta[0] and the
-#                      standard deviations of the state noise theta[1] and
-#                      observation noise theta[2].
-#
-# noParticles:         number of particles (N)
-#
-# initialState:        the initial state
-#
-# Outputs:
-# xHatFiltered:        vector with T elements
-#                      estimates of the filtered state
-#                      for each t=0,1,...,T-1.
-#
-# logLikelihood:       estimate of the log-likelihood at T-1
-#
-##############################################################################
-##############################################################################
+def particleFilter(observations, parameters, noParticles, initialState):
+        
+    noObservations = len(observations) - 1
+    phi = parameters[0]
+    sigmav = parameters[1]
+    sigmae = parameters[2]
 
-def particleFilter(y, theta, noParticles, initialState):
-
-    T = len(y)
-
-    # Initalise variables
-    particles = np.zeros((noParticles, T))
-    ancestorIndices = np.zeros((noParticles, T))
-    weights = np.zeros((noParticles, T))
-    normalisedWeights = np.zeros((noParticles, T))
-    xHatFiltered = np.zeros((T, 1))
-    logLikelihood = 0
+    particles = np.zeros((noParticles, noObservations))
+    ancestorIndices = np.zeros((noParticles, noObservations))
+    weights = np.zeros((noParticles, noObservations))
+    normalisedWeights = np.zeros((noParticles, noObservations))
+    xHatFiltered = np.zeros((noObservations, 1))
 
     # Set the initial state and weight
+    ancestorIndices[: , 0] = range(noParticles)
     particles[:, 0] = initialState
+    xHatFiltered[0] = initialState
     normalisedWeights[:, 0] = 1.0 / noParticles
-    weights[:, 0] = 1.0
+    logLikelihood = 0
 
-    #=====================================================================
-    # Run main loop
-    #=====================================================================
-    for t in range(1, T - 1):
+    # Main loop
+    for t in range(1, noObservations):
 
-        #=============================================================
-        # Resample particles
-        #=============================================================
+        # Resample (multinomial)
         newAncestors = choice(noParticles, noParticles, p=normalisedWeights[:, t - 1], replace=True)
-
-        # Resample the ancestory linage
         ancestorIndices[:, 1:t - 1] = ancestorIndices[newAncestors, 1:t - 1]
-
-        # Add the most recent ancestors
         ancestorIndices[:, t] = newAncestors
 
-        #=============================================================
-        # Propagate particles
-        #=============================================================
-        part1 = (theta[1]**(-2) + theta[2]**(-2))**(-1)
-        part2 = theta[2]**(-2) * y[t]
-        part2 = part2 + theta[1]**(-2) * theta[0] * particles[newAncestors, t - 1]
+        # Propagate
+        part1 = (sigmav**(-2) + sigmae**(-2))**(-1)
+        part2 = sigmae**(-2) * observations[t]
+        part2 = part2 + sigmav**(-2) * phi * particles[newAncestors, t - 1]
         particles[:, t] = part1 * part2 + np.sqrt(part1) * randn(1, noParticles)
 
-        #=================================================================
-        # Weight particles
-        #=================================================================
+        # Compute weights
+        yhatMean = phi * particles[:, t]
+        yhatVariance = np.sqrt(sigmav**2 + sigmae**2)
+        weights[:, t] = norm.logpdf(observations[t + 1], yhatMean, yhatVariance)
 
-        # Compute log-weights
-        yhatMean = theta[0] * particles[:, t]
-        yhatVariance = np.sqrt(theta[1]**2 + theta[2]**2)
-        weights[:, t] = norm.logpdf(y[t + 1], yhatMean, yhatVariance)
-
-        # Rescale log-weights and recover weights
         maxWeight = np.max(weights[:, t])
         weights[:, t] = np.exp(weights[:, t] - maxWeight)
-
-        # Normalise the weights
         sumWeights = np.sum(weights[:, t])
         normalisedWeights[:, t] = weights[:, t] / sumWeights
 
-        # Estimate the filtered state
+        # Estimate the state
         xHatFiltered[t] = np.sum(normalisedWeights[:, t] * particles[:, t])
 
         # Estimate log-likelihood
         predictiveLikelihood = maxWeight + np.log(sumWeights) - np.log(noParticles)
         logLikelihood += predictiveLikelihood
 
-    #=====================================================================
-    # Return state estimate and log-likelihood estimate
-    #=====================================================================
     return xHatFiltered, logLikelihood
 
-
-##############################################################################
-##############################################################################
-#
 # Bootstrap particle filter for the stochastic volatility model
-#
-# Inputs:
-# y:                   observations from the system for t=1,...,T.
-#
-# theta:               the mean of the state theta[0]
-#                      the persistence of the state theta[1] and the
-#                      standard deviations of the state noise theta[2].
-#
-# noParticles:         number of particles (N)
-#
-# Outputs:
-# stateTrajectory:     sample from the state filtering equation.
-#
-# logLikelihood:       estimate of the log-likelihood at T-1
-#
-##############################################################################
-##############################################################################
+def particleFilterSVmodel(observations, parameters, noParticles):
 
-def particleFilterSVmodel(y, theta, noParticles):
+    noObservations = len(observations)
+    mu = parameters[0]
+    phi = parameters[1]
+    sigmav = parameters[2]
 
-    T = len(y)
-
-    # Initalise variables
-    particles = np.zeros((noParticles, T))
-    ancestorIndices = np.zeros((noParticles, T))
-    weights = np.zeros((noParticles, T))
-    normalisedWeights = np.zeros((noParticles, T))
-    xHatFiltered = np.zeros((T, 1))
-    logLikelihood = 0
+    particles = np.zeros((noParticles, noObservations))
+    ancestorIndices = np.zeros((noParticles, noObservations))
+    weights = np.zeros((noParticles, noObservations))
+    normalisedWeights = np.zeros((noParticles, noObservations))
+    xHatFiltered = np.zeros((noObservations, 1))
 
     # Set the initial state and weight
-    particles[:, 0] = theta[0] + theta[2] / np.sqrt(1.0 - theta[1]**2) * randn(1, noParticles)
+    particles[:, 0] = mu + sigmav / np.sqrt(1.0 - phi**2) * randn(1, noParticles)
     normalisedWeights[:, 0] = 1.0 / noParticles
     weights[:, 0] = 1.0
+    logLikelihood = 0
 
-    #=====================================================================
-    # Run main loop
-    #=====================================================================
-    for t in range(1, T):
+    # Main loop
+    for t in range(1, noObservations):
 
-        #=============================================================
         # Resample particles
-        #=============================================================
         newAncestors = choice(noParticles, noParticles, p=normalisedWeights[:, t - 1], replace=True)
-
-        # Resample the ancestory linage
         ancestorIndices[:, 1:t - 1] = ancestorIndices[newAncestors, 1:t - 1]
-
-        # Add the most recent ancestors
         ancestorIndices[:, t] = newAncestors
 
-        #=============================================================
         # Propagate particles
-        #=============================================================
-        particles[:, t] = theta[0] + theta[1] * (particles[newAncestors, t - 1] - theta[0]) + theta[2] * randn(1, noParticles)
+        particles[:, t] = mu + phi * (particles[newAncestors, t - 1] - mu) + sigmav * randn(1, noParticles)
 
-        #=================================================================
         # Weight particles
-        #=================================================================
+        weights[:, t] = norm.logpdf(observations[t - 1], 0, np.exp(particles[:, t] / 2))
 
-        # Compute log-weights
-        weights[:, t] = norm.logpdf(y[t - 1], 0, np.exp(particles[:, t] / 2))
-
-        # Rescale log-weights and recover weights
         maxWeight = np.max(weights[:, t])
         weights[:, t] = np.exp(weights[:, t] - maxWeight)
-
-        # Normalise the weights
         sumWeights = np.sum(weights[:, t])
         normalisedWeights[:, t] = weights[:, t] / sumWeights
 
@@ -266,12 +135,9 @@ def particleFilterSVmodel(y, theta, noParticles):
         predictiveLikelihood = maxWeight + np.log(sumWeights) - np.log(noParticles)
         logLikelihood += predictiveLikelihood
 
-    #=====================================================================
-    # Return state estimate and log-likelihood estimate
-    #=====================================================================
-
+    
     # Sample the state estimate using the weights at t=T
-    ancestorIndex = choice(noParticles, 1, p=normalisedWeights[:, T - 1])
-    stateTrajectory = particles[ancestorIndices[ancestorIndex, T - 1].astype(int), :]
+    ancestorIndex = choice(noParticles, 1, p=normalisedWeights[:, noObservations - 1])
+    stateTrajectory = particles[ancestorIndices[ancestorIndex, noObservations - 1].astype(int), :]
 
     return stateTrajectory, logLikelihood
